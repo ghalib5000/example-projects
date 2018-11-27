aa69966c0173aca9b435887db2c8d4ade3e12fe5
ghalib5000 <ghalibahmed5000@gmail.com>
2018-11-28 02:30:21 +0500

    done with Q7_4

--- Q7_4/Program.cs
+++ Q7_4/Program.cs
@@ -6,7 +6,22 @@ namespace Q7_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int sum = 0,s2=0;
+            int[,] days = new int[8,7] {{ 2, 4, 3, 4, 5, 8, 8 }, //34      0
+                                         { 7, 3, 4, 3, 3, 4, 4 },//28      1
+                                         { 3, 3, 4, 3, 3, 2, 2 },//20      2
+                                         { 9, 3, 4, 7, 3, 4, 1 },//31      3
+                                         { 3, 5, 4, 3, 6, 3, 8 },//32      4
+                                         { 3, 4, 4, 6, 3, 4, 4 },//28      5
+                                         { 3, 7, 4, 8, 3, 8, 4 },//37      6
+                                         { 6, 3, 5, 9, 2, 7, 9 },//41      7
+            };
+            for (int i = 0; i < 8; i++)
+            {
+                sum = table.summer(days);
+                Console.WriteLine(sum);
+                sum = 0;
+            }
+            }
         }
-    }
 }
---
+++ Q7_4/table.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q7_4
+{
+    class table
+    {
+        public static int summer(int[,] days)
+        {
+            int sum = 0, s2 = 0;
+            int[] d = new int[7];
+            for (int p = 0; p < 8; p++)
+            {
+                for (int i = 0; i < 8; i++)//
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        s2 = s2 + days[i, j];
+                    }
+                    //   Console.WriteLine(s2);//sum
+                    if (s2 > sum)
+                    {
+                         for(int u=0;u<7;u++)
+                        {
+                            d[u] = days[i, u];
+                        }
+                        sum = s2;
+                        s2 = 0;
+                    }
+                    s2 = 0;
+                }                        //
+                s2 = 0;
+                int[] d2 = new int[7];
+                for (int i = 0; i < 8; i++)
+                {
+                    for (int j = 0; j < 7; j++)
+                    {
+                        s2 = s2 + days[i, j];
+                        d2[j] = days[i, j];
+                    }
+                    if ((s2 == sum) && (d2[0] == d[0]  && d2[1] == d[1] && d2[2] == d[ 2] && d2[3] == d[ 3] && d2[4] == d[ 4] && d2[5] == d[ 5] && d2[6] == d[ 6]))
+                    {
+                        for (int u = 0; u < 7; u++)
+                        {
+                            days[i, u] = 0;
+                            s2 = 0;
+                        }
+                    }
+                    s2 = 0;
+                }
+            }
+            return sum;
+        }
+    }
+}
