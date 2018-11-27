70dc999ede2f668b84f64d10b546855b3131b22f
ghalib5000 <ghalibahmed5000@gmail.com>
2018-11-27 18:57:46 +0500

    added assignment file

--- Q6_29/Program.cs
+++ Q6_29/Program.cs
@@ -37,7 +37,7 @@ namespace Q6_29
                     n3 = 1;
                 }
                 n1++;
-                n2 = n1;
+                n2 = 1;
             }
             Console.WriteLine("the total ways of getting sum of 24 from 4 cards are" +
                 ": " + count);
