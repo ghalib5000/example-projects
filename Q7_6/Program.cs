4371f509ec392c6a02467d17cb3e595d07c8b93c
ghalib5000 <ghalibahmed5000@gmail.com>
2018-11-28 16:12:11 +0500

    done matrix

---
+++ Q7_6/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Q7_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[,] a = new double[3, 3] { { 1, 2, 3 }, { 4, 5, 6 }, { 7, 8, 9 } };
+            double[,] b = new double[3, 3] { { 0, 2, 4 }, { 1, 4.5, 2.2 }, { 1.1, 4.3, 5.2 } };
+            double[,] c = new double[3, 3];
+            double[,] r = new double[3, 9];
+            /*  Console.WriteLine("enter matrix 1 values: ");
+              a = matrix.numtaker();
+              Console.WriteLine("enter matrix 2 values: ");
+              b = matrix.numtaker();*/
+            c = matrix.result(a, b);
+            matrix.dispmat(a);
+            Console.WriteLine("  X");
+            matrix.dispmat(b);
+            Console.WriteLine("  =");
+            matrix.dispmat(c);
+            Console.WriteLine("");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (j < 3)
+                    {
+                        r[i, j] = a[i, j];
+                    }
+                    else if (j < 6 && j > 2)
+                    {
+                        r[i, j] = b[i, j-3];
+                    }
+                    if (j < 9 && j > 5)
+                    {
+                        r[i, j] = c[i, j-6];
+                    }
+                }
+            }
+            matrix.dispmatr(r);
+        }
+    }
+}
---
+++ Q7_6/matrix.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q7_6
+{
+    class matrix
+    {
+        public static double[,] result(double[,] a, double[,] b)
+        {
+            double[,] c = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    c[i, j] = (a[i, 0] * b[0, j]) + (a[i, 1] * b[1, j])+(a[i, 2] * b[2, j]);
+                }
+            }
+            return c;
+        }
+        public static void dispmat(double[,]a)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(a[i, j]+" ");
+                }
+                Console.WriteLine("");
+            }
+        }
+        public static void dispmatr(double[,] a)
+        {
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+                    if (i == 1 && j == 3)
+                    {
+                        Console.Write(" x ");
+                    }
+                    if (i == 1 && j == 6)
+                    {
+                        Console.Write(" = ");
+                    }
+                    if (i != 1 && (j == 3||j==6))
+                    {
+                        Console.Write("   ");
+                    }
+                    Console.Write("  "+a[i, j] + " ");
+                }
+                Console.WriteLine(""); Console.WriteLine("");
+            }
+        }
+        public static double[,] numtaker()
+        {
+            double[,] a = new double[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    a[i, j] = double.Parse(Console.ReadLine());
+                }
+            }
+            return a;
+        }
+    }
+}
