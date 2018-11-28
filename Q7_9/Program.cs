a806f846a9cec9c634f23f1f016e66337ed208f1
ghalib5000 <ghalibahmed5000@gmail.com>
2018-11-28 18:58:40 +0500

    cone tic tac toe Q7_9

--- Q7_9/Program.cs
+++ Q7_9/Program.cs
@@ -6,7 +6,64 @@ namespace Q7_9
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Random rand = new Random();
+
+            int row, col, turns = 0, c=0;
+            row = rand.Next(0, 2);
+            col = rand.Next(0, 2);
+            char[,] game = new char[3, 3];
+            tic.tac(game);
+            for (int u = 0; u < 3; u++)
+            {
+                for (int y = 0; y < 3; y++)
+                {
+                    while (game[u, y] == 0)
+
+                    {
+                       // row = rand.Next(0, 3);
+                       // col = rand.Next(0, 3);
+                              Console.WriteLine("Enter a row(0,1 or 2) for player 1:");
+                              row = int.Parse(Console.ReadLine());
+                              Console.WriteLine("Enter a column(0,1 or 2) for player 1:");
+                              col = int.Parse(Console.ReadLine());
+                        tic.valcheck(game, row, col, 'O');
+                        tic.tac(game);
+                        c = tic.checker1(game);
+                        if (c == 15)
+                        {
+                            y = 5;
+                            u = 5;
+                            break; }
+                        c = tic.checker2(game);
+                        if (c == 15)
+                        { y = 5; u = 5; break; }
+                        c = tic.checker3(game);
+                        if (c == 15)
+                        { y = 5; u = 5; break; }
+                        //PLAYER 2 HERE
+                      //  row = rand.Next(0, 3);
+                       // col = rand.Next(0, 3);
+                          Console.WriteLine("Enter a row(0,1 or 2) for player 2:");
+                          row = int.Parse(Console.ReadLine());
+                          Console.WriteLine("Enter a column(0,1 or 2) for player 2:");
+                          col = int.Parse(Console.ReadLine());
+                        tic.valcheck(game, row, col, 'X');
+                        tic.tac(game);
+                        c = tic.checker1(game);
+                        if (c == 15)
+                        { y = 5; u = 5; break; }
+                        c = tic.checker2(game);
+                        if (c == 15)
+                        { y = 5; u = 5; break; }
+                        c = tic.checker3(game);
+                        if (c == 15)
+                        { y = 5; u = 5; break; }
+                        turns++;
+                    }
+                }
+            }
+            if(c==0)
+            { Console.WriteLine("it's a draw"); }
         }
     }
 }
---
+++ Q7_9/tic.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q7_9
+{
+    class tic
+    {
+        public static void tac(char[,] game)
+        {
+            Console.WriteLine("-------------");
+            Console.Write("|");
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(" " + game[i, j] + " |");
+                }
+                Console.WriteLine("");
+                Console.Write("-------------");
+                Console.WriteLine("");
+                Console.Write("|");
+            }
+        }
+        public static void valcheck(char[,] game, int row, int col, char c)
+        {
+            if (game[row, col] == 0)
+            {
+                game[row, col] = c;
+            }
+            else
+            {
+                Console.WriteLine("you tried to overwrite the older value which resulted in your turn being missed");
+            }
+        }
+        public static int checker1(char[,] game)
+        {
+            int turns = 0;
+            int p1 = 0, p2 = 0, i, j;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (game[i, j] == 'O')
+                    {
+                        p1++;
+                    }
+                    else if (game[i, j] == 'X')
+                    {
+                        p2++;
+                    }
+                    if (p1 == 3)
+                    { //player O wins
+                        Console.WriteLine("player 1 wins");
+                        turns = 15;
+                        return turns;
+                    }
+                    else if (p2 == 3)
+                    {
+                        //player X wins
+                        Console.WriteLine("player 2 wins");
+                        turns = 15;
+                        return turns;
+                    }
+                }
+                p1 = 0;
+                p2 = 0;
+
+            }
+            return turns;
+        }
+        public static int checker2(char[,] game)
+        {
+            int turns = 0;
+            int p1 = 0, p2 = 0, i, j;
+            for (i = 0; i < 3; i++)
+            {
+                for (j = 0; j < 3; j++)
+                {
+                    if (game[j, i] == 'O')
+                    {
+                        p1++;
+                    }
+                    else if (game[j, i] == 'X')
+                    {
+                        p2++;
+                    }
+                    if (p1 == 3)
+                    { //player O wins
+                        Console.WriteLine("player 1 wins");
+                        turns = 15;
+                        return turns;
+                    }
+                    else if (p2 == 3)
+                    {
+                        //player X wins
+                        Console.WriteLine("player 2 wins");
+                        turns = 15;
+                        return turns;
+                    }
+                }
+                p1 = 0;
+                p2 = 0;
+
+            }
+            return turns;
+        }
+        public static int checker3(char[,] game)
+        {              //we need 0,0 1,1 2,2 0,2 2,0
+            int turns = 0;
+            int p1 = 0, p2 = 0, i, j;
+            for (i = 0; i < 3; i++)
+            {
+                    if (game[i, i] == 'O')
+                    {
+                        p1++;
+                    }
+                    else if (game[i, i] == 'X')
+                    {
+                        p2++;
+                    }
+                    else if(i==0)
+                    {
+                    p1 = 0;
+                    p2 = 0;
+                        if(game[i,2]=='O')
+                        {
+                            p1++;
+                        }
+                        else if (game[i, 2] == 'X')
+                        {
+                            p2++;
+                        }
+                        else if (game[2, i] == 'O')
+                        {
+                            p1++;
+                        }
+                        else if (game[2, i] == 'X')
+                        {
+                            p2++;
+                        }
+                    }
+                    if (p1 == 3)
+                    { //player O wins
+                        Console.WriteLine("player 1 wins");
+                        turns = 15;
+                        return turns;
+                    }
+                    else if (p2 == 3)
+                    {
+                        //player X wins
+                        Console.WriteLine("player 2 wins");
+                        turns = 15;
+                        return turns;
+                    }
+            }
+            return turns;
+        } 
+    }
+}
