19001dbeee009fb468ce284d6a02def8caae42b1
ghalib5000 <ghalibahmed5000@gmail.com>
2018-11-29 22:26:55 +0500

    done Q7_19

--- Q7_17/Program.cs
+++ Q7_17/Program.cs
@@ -5,8 +5,51 @@ namespace Q7_17
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
+        {//                                 banks ammount
+            int[] bankammounts = new int[5] { 25, 125, 175, 75, 181 };
+            string[] nosafe=new string[5];
+            double[,] bor = new double[5, 5];
+            double[] bankam = new double[5];
+            bor[0, 1] = 100.5;
+            bor[0, 4] = 320.5;
+            bor[1, 2] = 40;
+            bor[1, 3] = 85;
+            bor[2, 0] = 125;
+            bor[2, 3] = 75;
+            bor[3, 0] = 125;
+            bor[4, 2] = 125;
+            int n = 5, limit = 201, count;
+            double borrow;
+              Console.Write("enter number of banks:");
+              n = int.Parse(Console.ReadLine());
+              Console.Write("enter limit amount:");
+              limit = int.Parse(Console.ReadLine());
+            for(int i=0;i<n;i++)
+            {
+                Console.Write("Bank no#"+i+": "+bankammounts[i]+" ");
+                count = bank.countbor(bor, i);
+                Console.Write(count+" ");
+                for(int y=0;y<n;y++)
+                {
+                    borrow = bank.loanfind(bor, i,y);
+                    for(int t=0;t<n;t++)
+                    {
+                        if(bor[i,t]==borrow&&borrow!=0)
+                        {
+                            Console.Write(t+" ");
+                        }
+                    }
+                    if(borrow!=0)
+                    Console.Write(borrow+" ");
+                }
+                Console.WriteLine("");
+            }
+            nosafe = bank.loansub(n, limit, bankam, bor, bankammounts);
+            foreach (string y in nosafe)
+            {
+                if(y != null)
+                Console.WriteLine(y);
+            }
         }
     }
 }
---
+++ Q7_17/bank.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q7_17
+{
+    class bank
+    {
+        public static double totalass(int banknum,double[,] bor,int[] bankn)
+        {
+            double total = bankn[banknum];
+            int i =0;
+            while(i<5)
+            {
+                total = total + bor[banknum, i];
+                i++;
+            }
+            return total;
+        }
+        public static string[] loansub(int n,int limit,double[] bankam,double[,] bor,int[] bankammounts)
+        {
+            int u = 0, i = 0;
+            string[] nosafe=new string[5];
+            while (u < n)
+            {
+                while (i < n)
+
+                {
+                    bankam[i] = bank.totalass(i, bor, bankammounts);
+                    if (bankam[i] < limit)
+                    {
+                        nosafe[i] = "bank " + i + "  is unsafe it's ammount is: " + bankam[i];
+                        bankam[i] = limit + 1;
+                        for (int j = 0; j < 5; j++)
+                        {
+                            bankam[j] = bankam[j] - bor[j, i];
+                            bor[j, i] = 0;
+                        }
+                    }
+                    i++;
+                }
+                u++;
+                i = 0;
+            }
+            return nosafe;
+        }
+
+        public static int countbor(double[,] bor,int banknum)
+        {
+            int count=0;
+            for(int i = 0;i<5;i++)
+            {
+                if(bor[banknum,i]>0)
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        public static double loanfind(double[,] bor, int banknum,int y)
+        {
+
+            double count = 0;
+            for (int i = 0; i <= y; i++)
+            {
+                if (bor[banknum, y] > 0)
+                {
+                    count = bor[banknum, y];
+                    return count;
+                }
+            }
+            return count;
+        }
+    }
+}
