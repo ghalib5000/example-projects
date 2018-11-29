e4acefd1f5f71b0cf79daf8352de1f815b9acf4a
ghalib5000 <ghalibahmed5000@gmail.com>
2018-11-29 18:34:03 +0500

    done Q7_11

--- Q7_11/Program.cs
+++ Q7_11/Program.cs
@@ -6,7 +6,21 @@ namespace Q7_11
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[,] mat = new int[3, 3];
+            char[,] ch = new char[3, 3];
+            int num;
+            Console.Write("enter number between 0-511: ");
+            num = int.Parse(Console.ReadLine());
+            mat = convertor.dectobin(num);
+            ch = convertor.numtochar(mat);
+             for (int i = 0; i < 3; i++)
+                {
+                for (int j = 0; j < 3; j++)
+                {
+                 Console.Write(ch[i, j]+" ");
+                }
+                Console.WriteLine("");
+                }
         }
     }
 }
---
+++ Q7_11/convertor.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q7_11
+{
+    class convertor
+    {
+        public static int[,] dectobin(int a)
+        {//if divided value is greater then int.pasre(value) then the is 1 remainder
+            double rem = a, r;
+            int i = 2, j = 2;
+            int[,] tab = new int[3, 3];
+            while (i >= 0)
+            {
+                while (j >= 0)
+                {
+                    if (rem == 1)
+                    {
+                        tab[i, j] = 1;
+                        return tab;
+                    }
+                    else if (rem % 2 == 0)
+                    {
+                        rem = rem / 2;
+                        tab[i, j] = 0;
+                    }
+                    else if (rem % 2 != 0)
+                    {
+                        rem = (rem - 1) / 2;
+                        tab[i, j] = 1;
+                    }
+                    j--;
+                }
+                i--;
+                j = 2;
+            }
+            return tab;
+        }    
+        public static char[,] numtochar(int[,] a)
+        {
+            char[,] ht = new char[3, 3];
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    if(a[i,j]==0)
+                    {//head
+                        ht[i, j] = 'H';
+                    }
+                    else if (a[i, j] == 1)
+                    {//tail
+                        ht[i, j] = 'T';
+                    }
+                }
+            }
+            return ht;
+        }
+    }
+}
+   
+
