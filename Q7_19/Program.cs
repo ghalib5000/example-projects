cc6b8d82b5b353dee2821a3e5a30dcd0b9c446fb
ghalib5000 <ghalibahmed5000@gmail.com>
2018-11-30 17:44:24 +0500

    done basic of Q7_19

---
+++ Q7_19/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Q7_19
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, col;
+            Console.Write("Enter rows: ");
+            row = int.Parse(Console.ReadLine());
+            Console.Write("Enter columns: ");
+            col = int.Parse(Console.ReadLine());
+            Console.WriteLine("");
+            int[,] mat = new int[row, col];
+            for(int i=0;i<row;i++)
+            {
+                for(int j=0;j<col;j++)
+                {
+                      mat[i, j] = pattern.randnum(row, col);
+                 //   Console.Write("enter value for row {0} column {1}: ",i,j);
+                  //  mat[i, j] = int.Parse(Console.ReadLine());
+                }
+            }
+            mat[1, 4] = 9;
+            mat[2, 4] = 9;
+            mat[3, 4] = 9;
+            mat[4, 4] = 9;
+            mat[2, 1] = 5;
+            mat[2, 2] = 5;
+            mat[2, 3] = 5;
+            mat[2, 0] = 5;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    Console.Write(mat[i, j] + " ");
+                }
+                Console.WriteLine("");
+            }
+            pattern.checker1(row, col, mat);
+            pattern.checker2(row, col, mat);
+           // pattern.checker3(row, col, mat);
+        }
+    }
+}
---
+++ Q7_19/pattern.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Q7_19
+{
+    class pattern
+    {
+        public static int randnum(int row, int col)
+        {
+            Random r = new Random();
+            int rand = r.Next(0, 9);
+            return rand;
+        }
+        public static void checker1(int row, int col, int[,] mat)
+        {
+
+            int p1 = 0, i, j, n;
+            for (n = 0; n < 10; n++)
+            {
+                for (i = 0; i < row; i++)
+                {
+                    for (j = 0; j < col; j++)
+                    {
+                        if (mat[i, j] == n)
+                        {
+                            p1++;
+                        }
+                        else
+                        {
+                            p1 = 0;
+                        }
+                        if (p1 == 4)
+                        { //player O wins
+                            Console.WriteLine("the common number is: " + n);
+                            p1 = 0;
+                        }
+                    }
+                    p1 = 0;
+                }
+            }
+        }
+        public static void checker2(int row, int col, int[,] mat)
+        {
+            int p1 = 0, i, j, n;
+            for (n = 0; n < 10; n++)
+            {
+                for (i = 0; i < 3; i++)
+                {
+                    for (i = 0; i < col; i++)
+                    {
+                        for (j = 0; j < row; j++)
+                        {
+                            if (mat[j, i] == n)
+                            {
+                                p1++;
+                            }
+                            else
+                            {
+                                p1 = 0;
+                            }
+                            if (p1 == 4)
+                            { //player O wins
+                                Console.WriteLine("the common number is: " + n);
+                                p1 = 0;
+                            }
+                        }
+                        p1 = 0;
+                    }
+                }
+            }
+        }
+        public static void checker3(int row, int col, int[,] mat)
+        {              //we need 0,0 1,1 2,2 0,2 2,0
+
+            int p1 = 0, i, j, n;
+            for (n = 0; n < 10; n++)
+            {
+                for (i = 0; i < row; i++)
+                {
+                    for (j = col-1; j >= 0; j--)
+                    {
+                        if (mat[i, i] == n)
+                        {
+                            p1++;
+                        }
+                        else if (mat[j, j] == n)
+                        {
+                            p1++;
+                        }
+                        else if (mat[i, j] == n)
+                        {
+                            p1++;
+                        }
+                        else
+                        {
+                            p1 = 0;
+                        }
+                        if (p1 == 4)
+                        { //player O wins
+                            Console.WriteLine("the common number is: " + n);
+                            p1 = 0;
+                        }
+                    }
+                    p1 = 0;
+                }
+            }
+        }
+    }
+}
